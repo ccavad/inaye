f6c0911befbbe2de3d97b88651ba638debefd082
ccavad <ccavad2015@gmail.com>
2022-10-12 14:00:49 +0400

    homepage

--- main.js
+++ main.js
@@ -1,7 +1,60 @@
+// dom elements
+const scrollTopBtn = document.querySelector(".scrollTopBtn");
+
 // swiper
-let swiper = new Swiper(".home__hero", {
+let swiperHero = new Swiper(".home__hero", {
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+let swiperActivities = new Swiper(".home__activities-swiper", {
+  autoplay: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let swiperFooter = new Swiper(".footer-swiper", {
+  autoplay: true,
+  slidesPerView: 5,
+  // spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// scroll
+
+window.addEventListener("scroll", () => {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
